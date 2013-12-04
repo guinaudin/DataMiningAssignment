fca64ec5deefec1dd8885b22bbbce9182dd34aa5
Guillaume Naudin <guillaume.naudin78@gmail.com>
2013-12-04 10:05:28 +0100

    clean commentaries

--- src/tweetstats/view/mainView.java
+++ src/tweetstats/view/mainView.java
@@ -1,13 +1,5 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package tweetstats.view;
 
-/**
- *
- * @author guillaumenaudin
- */
 public class mainView {
     
 }
